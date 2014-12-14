504f76556661bce7e9a2a0a33000efef4f051d0f
Zhao Han <cx.chenghai+github@gmail.com>
2014-12-14 00:02:46 -0600

    Fix a tab indent in ionicons-rails.rb

--- lib/ionicons-rails.rb
+++ lib/ionicons-rails.rb
@@ -1,7 +1,7 @@
 module Ionicons
   module Rails
     class Engine < ::Rails::Engine
-    	initializer "ionicons-rails.assets.precompile" do |app|
+      initializer "ionicons-rails.assets.precompile" do |app|
         app.config.assets.precompile << %r(ionicons\.(?:eot|svg|ttf|woff)$)
       end
     end
